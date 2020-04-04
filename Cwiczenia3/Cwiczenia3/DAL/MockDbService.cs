0ae0f3e80af3fccdc4bd4bbf00bcebda6b0e34c6
Jan Rejnowski <jan.rejnowski@gmail.com>
2020-04-04 16:49:15 +0200

    Deleted unnecessary field IdStudent

--- Cwiczenia3/Cwiczenia3/DAL/MockDbService.cs
+++ Cwiczenia3/Cwiczenia3/DAL/MockDbService.cs
@@ -15,9 +15,9 @@ namespace Cwiczenia3.DAL
         {
             _students = new List<Student>
             {
-                new Student{IdStudent=1, FirstName="Jan", LastName="Kowalski"},
-                new Student{IdStudent=2, FirstName="Anna", LastName="Malewski"},
-                new Student{IdStudent=3, FirstName="Andrzej", LastName="Andrzejewicz"},
+                new Student{IndexNumber="1", FirstName="Jan", LastName="Kowalski"},
+                new Student{IndexNumber="3", FirstName="Anna", LastName="Malewski"},
+                new Student{IndexNumber="4", FirstName="Andrzej", LastName="Andrzejewicz"},
             };
         }
 
